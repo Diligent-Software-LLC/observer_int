d1a5df0e62ae0a0600a1db22287dcbb27cff8653
Bradley J. Tannor <bradleytannor@gmail.com>
2020-05-04 09:34:58 -0700

    Development Complete
    Added 'ObserverInt.observable()', 'ObserverInt.type(identifier = nil)', 'ObserverInt.changed(instance = nil)', 'ObserverInt.notify(instance = nil)', and 'ObserverInt.kind_observer(instance = nil)'. Declared 'new()' and 'ObserverInt.kind_observer(instance = nil)' private. Added method declaration test coverage.

--- lib/observer_int.rb
+++ lib/observer_int.rb
@@ -4,8 +4,62 @@
 require_relative "observer_int/version"
 
 # ObserverInt.
+# @abstract
 # @class_description
 #   An Observer library interface.
 class ObserverInt
-end
 
+  # self.observable().
+  # @description
+  #   Gets OBSERVABLE.
+  # @return [Set]
+  #   OBSERVABLE's reference.
+  def self.observable()
+  end
+
+  # self.type(identifier = nil).
+  # @description
+  #   Predicate. Verifies an identifier is an Observable identifier.
+  # @param identifier [.]
+  #   Any identifier.
+  # @return [TrueClass, FalseClass]
+  #   True in the case the identifier is an Observable type. False otherwise.
+  def self.type(identifier = nil)
+  end
+
+  # self.changed(instance = nil).
+  # @description
+  #   Verifies an instance's state.
+  # @param instance [.]
+  #   Any instance.
+  # @return [TrueClass, FalseClass]
+  #   True in the case the argument is an Observable type, it is an
+  #   Observer's subject, and its state changed. False otherwise.
+  def self.changed(instance = nil)
+  end
+
+  # self.notify(instance = nil).
+  # @description
+  #   Notifies the appropriate Observer kind an instance's state changed.
+  # @param instance [.]
+  #   An Observable instance.
+  # @return [NilClass]
+  #   nil.
+  def self.notify(instance = nil)
+  end
+
+  # self.kind_observer(instance = nil).
+  # @description
+  #   Gets the corresponding type's observer.
+  # @param instance [.]
+  #   Any instance.
+  # @return [.]
+  #   The argument's corresponding observer identifier.
+  def self.kind_observer(instance = nil)
+  end
+
+  # Private class methods.
+  private_class_method :new
+  private_class_method :kind_observer
+
+end
--- test/observer_int_test.rb
+++ test/observer_int_test.rb
@@ -5,10 +5,13 @@ require_relative 'test_helper'
 #   Tests the ObserverInt class.
 class ObserverIntTest < Minitest::Test
 
+  # Constants.
+  CLASS = ObserverInt
+
   # test_conf_doc_f_ex().
   # @description
-  #  The .travis.yml, CODE_OF_CONDUCT.md, Gemfile, LICENSE.txt, README.md, 
-  #  and .yardopts files exist.
+  #   The .travis.yml, CODE_OF_CONDUCT.md, Gemfile, LICENSE.txt, README.md,
+  #   and .yardopts files exist.
   def test_conf_doc_f_ex()
 
     assert_path_exists('.travis.yml')
@@ -31,13 +34,36 @@ class ObserverIntTest < Minitest::Test
   # @description
   #   Set fixtures.
   def setup()
+    @pub_cm  = CLASS.public_methods(false)
+    @priv_cm = CLASS.private_methods(false)
+  end
+
+  # test_pubcm_dec().
+  # @description
+  #   'ObserverInt.observable()', 'ObserverInt.changed(instance = nil)', and
+  #   'ObserverInt.notify(instance = nil)' were declared.
+  def test_pubcm_dec()
+
+    assert_includes(@pub_cm, :observable)
+    assert_includes(@pub_cm, :changed)
+    assert_includes(@pub_cm, :notify)
+    assert_includes(@pub_cm, :type)
+
+  end
+
+  # test_privcm_dec().
+  # @description
+  #   'ObserverInt.kind_observer(instance = nil)', and 'ObserverInt.new()' were
+  #   declared.
+  def test_privcm_dec()
+    assert_includes(@priv_cm, :kind_observer)
+    assert_includes(@priv_cm, :new)
   end
 
   # teardown().
   # @description
-  #  Cleanup.
+  #   Cleanup.
   def teardown()
   end
 
 end
-
